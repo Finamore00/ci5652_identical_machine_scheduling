15819968aa46ad2a9c5d324a6d51999b44d4cef8
Ana <73360276+anna-ayn@users.noreply.github.com>
2024-05-16 19:26:50 -0400

    time

--- src/source_files/main.cpp
+++ src/source_files/main.cpp
@@ -4,12 +4,14 @@
 #include <utility>
 #include <string>
 #include <filesystem>
+#include <chrono>
 #include "../header_files/Job.h"
 #include "../header_files/utilities.h"
 #include "../header_files/heuristic.h"
 #include "../header_files/exact.h"
 #include "../header_files/local_search.h"
 
+using namespace std::chrono;
 
 int main() {
     // for each file of benchmarks (n=20, m=2), read the file and run the MDD algorithm
@@ -27,30 +29,46 @@ int main() {
         }
 
         // Run the MDD algorithm for the TT problem with n jobs and m machines
-        vector<vector<Job>> heuristic_schedule = mddScheduling(jobs, m);
-        // vector<vector<Job>> exact_schedules = exact_solution(jobs, m);
-        vector<vector<Job>> local_search_schedule = local_search(jobs, m, 25000);
 
-        cout << "Heuristic Schedule" << endl;
+        auto start = high_resolution_clock::now();
+        vector<vector<Job>> heuristic_schedule = mddScheduling(jobs, m);
+        auto end = high_resolution_clock::now();
+        std::chrono::duration<double>  duration_h = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
+        
+        // start = high_resolution_clock::now(); 
+        // vector<vector<Job>> exact_schedules = exact_solution(jobs, m);
+        // end = high_resolution_clock::now();
+        // std::chrono::duration<double> duration_e = duration_cast<microseconds>(end - start);
+
+        start = high_resolution_clock::now();
+        vector<vector<Job>> local_search_schedule = local_search(jobs, m, 25000);
+        end = high_resolution_clock::now();
+        std::chrono::duration<double> duration_ls = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
+
+
+        cout << "❤️ Heuristic Schedule" << endl;
         print_schedule(heuristic_schedule, m);
 
         // calculate total tardiness
         long long total_tardiness_heuristic = total_tardiness(heuristic_schedule);
         cout << "\nTotal Tardiness: " << total_tardiness_heuristic << endl;
+        cout << "Time taken by heuristic: " << duration_h.count() << " seconds\n" << endl;
 
-        // cout << "Exact Schedule" << endl;
+        // cout << "🙈 Exact Schedule" << endl;
         // print_schedule(exact_schedules, m);
 
-        // // calculate total tardiness
+        // calculate total tardiness
         // long long exact_total_tardiness = total_tardiness(exact_schedules);
         // cout << "\nTotal Tardiness: " << exact_total_tardiness << endl;
+        // cout << "Time taken by exact algorithm: " << duration_e.count() << " seconds\n" << endl;
 
-        cout << "Local Search Schedule" << endl;
+        cout << "✨ Local Search Schedule" << endl;
         print_schedule(local_search_schedule, m);
 
         //calculate total tardiness
         long long local_search_total_tardiness = total_tardiness(local_search_schedule);
         cout << "\nTotal Tardiness: " << local_search_total_tardiness << endl;
+        cout << "Time taken by local search: " << duration_ls.count() << " seconds\n\n" << endl;
     }
 
     return 0;
