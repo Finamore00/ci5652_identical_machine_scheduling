7692ec413f447a2865a85d12762140355ce4178b
Ana <73360276+anna-ayn@users.noreply.github.com>
2024-05-16 20:14:28 -0400

    only local search

--- src/source_files/main.cpp
+++ src/source_files/main.cpp
@@ -13,9 +13,9 @@
 
 using namespace std::chrono;
 
-int main() {
+int main(int argc, char *argv[]) {
     // for each file of benchmarks (n=20, m=2), read the file and run the MDD algorithm
-    string path = "../benchmarks/n=20/m=2/";
+    string path = argv[1];
     
     for (const auto & entry : filesystem::directory_iterator(path)) {
         // store the information of the file
@@ -30,29 +30,29 @@ int main() {
 
         // Run the MDD algorithm for the TT problem with n jobs and m machines
 
-        auto start = high_resolution_clock::now();
-        vector<vector<Job>> heuristic_schedule = mddScheduling(jobs, m);
-        auto end = high_resolution_clock::now();
-        std::chrono::duration<double>  duration_h = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
+        // auto start = high_resolution_clock::now();
+        // vector<vector<Job>> heuristic_schedule = mddScheduling(jobs, m);
+        // auto end = high_resolution_clock::now();
+        // std::chrono::duration<double>  duration_h = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
         
         // start = high_resolution_clock::now(); 
         // vector<vector<Job>> exact_schedules = exact_solution(jobs, m);
         // end = high_resolution_clock::now();
-        // std::chrono::duration<double> duration_e = duration_cast<microseconds>(end - start);
+        // std::chrono::duration<double>  duration_e = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
 
-        start = high_resolution_clock::now();
+        auto start = high_resolution_clock::now();
         vector<vector<Job>> local_search_schedule = local_search(jobs, m, 25000);
-        end = high_resolution_clock::now();
+        auto end = high_resolution_clock::now();
         std::chrono::duration<double> duration_ls = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
 
 
-        cout << "❤️ Heuristic Schedule" << endl;
-        print_schedule(heuristic_schedule, m);
+        // cout << "❤️ Heuristic Schedule" << endl;
+        // print_schedule(heuristic_schedule, m);
 
         // calculate total tardiness
-        long long total_tardiness_heuristic = total_tardiness(heuristic_schedule);
-        cout << "\nTotal Tardiness: " << total_tardiness_heuristic << endl;
-        cout << "Time taken by heuristic: " << duration_h.count() << " seconds\n" << endl;
+        // long long total_tardiness_heuristic = total_tardiness(heuristic_schedule);
+        // cout << "\nTotal Tardiness: " << total_tardiness_heuristic << endl;
+        // cout << "Time taken by heuristic: " << duration_h.count() << " seconds\n" << endl;
 
         // cout << "🙈 Exact Schedule" << endl;
         // print_schedule(exact_schedules, m);
@@ -65,7 +65,7 @@ int main() {
         cout << "✨ Local Search Schedule" << endl;
         print_schedule(local_search_schedule, m);
 
-        //calculate total tardiness
+        // calculate total tardiness
         long long local_search_total_tardiness = total_tardiness(local_search_schedule);
         cout << "\nTotal Tardiness: " << local_search_total_tardiness << endl;
         cout << "Time taken by local search: " << duration_ls.count() << " seconds\n\n" << endl;
