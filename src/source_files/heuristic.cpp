ae91627b123b703de71a70c0db51e391131cfa57
Ana <73360276+anna-ayn@users.noreply.github.com>
2024-05-19 13:42:53 -0400

    splitting

--- src/source_files/heuristic.cpp
+++ src/source_files/heuristic.cpp
@@ -49,9 +49,6 @@ void partition_jobs(vector<Job *> unscheduled_jobs, long long processed, vector<
 
 */
 pair<Job*, long long> find_min_mdd_job(vector<Job*> unscheduled_jobs, long long processed) {
-    /*
-        Find the job with the minimum MDD value from the unscheduled jobs.
-    */
     long long min_mdd = LLONG_MAX; 
     Job* min_mdd_job;
 
@@ -67,6 +64,45 @@ pair<Job*, long long> find_min_mdd_job(vector<Job*> unscheduled_jobs, long long 
 }
 
 /*
+    Find the machine with the minimum delta value.
+    Args:
+        - jobs is a list of all unscheduled jobs
+        - numMachines is the number of machines
+        - completion_times is a list of completion times for all machines
+        - min_delta_machine is the machine with the minimum delta value
+        - min_delta_job is the job with the minimum delta value
+*/
+void find_gl(vector<Job *> jobs, int numMachines, vector<long long> completion_times, int &min_delta_machine, Job* &min_delta_job) {
+    long long min_delta = LLONG_MAX;
+    for (int machine = 0; machine < numMachines; machine++) {
+        vector<Job *> u1, u2; // Initialize u1 and u2
+        partition_jobs(jobs, completion_times[machine], u1, u2);
+
+        long long min_process_time = LLONG_MAX;
+        long long min_due_date = LLONG_MAX;
+        for (Job* job : u1)
+            min_process_time = min(min_process_time, job->ptime);
+        for (Job* job : u2)
+            min_due_date = min(min_due_date, job->dDate);
+
+        vector<Job*> gamma_lambda;
+        for (Job* job : u1) 
+            if (job->ptime == min_process_time)
+                gamma_lambda.push_back(job);
+        for (Job* job : u2) 
+            if (job->dDate == min_due_date) 
+                gamma_lambda.push_back(job);
+
+        pair<Job*, long long> min_mdd_job = find_min_mdd_job(gamma_lambda, completion_times[machine]);
+        if (min_mdd_job.second < min_delta) {
+            min_delta = min_mdd_job.second;
+            min_delta_machine = machine;
+            min_delta_job = min_mdd_job.first;
+        }
+    }
+}
+
+/*
     Implementation of the MDD algorithm for the TT (Total Tardiness) problem with multiple identical machines.
     Args:
         - jobs is a list of all jobs
@@ -77,48 +113,16 @@ pair<Job*, long long> find_min_mdd_job(vector<Job*> unscheduled_jobs, long long 
 */
 vector<vector<Job*>> mddScheduling(vector<Job *> jobs, int numMachines) {
     vector<long long> completion_times(numMachines, 0);  // Initialize completion times for all machines
-
     vector<vector<Job *>> schedules(numMachines);  // Initialize schedules for all machines
 
     // While there are unscheduled jobs
     while (!jobs.empty()) {
-        long long min_delta = LLONG_MAX;
+        
         int min_delta_machine = -1;
         Job* min_delta_job;
 
-        for (int machine = 0; machine < numMachines; machine++) {
-            vector<Job *> u1, u2; // Initialize u1 and u2
-            partition_jobs(jobs, completion_times[machine], u1, u2);
-
-            // find the minimum processing time and due date for u1 and u2, respectively
-            long long min_process_time = LLONG_MAX;
-            long long min_due_date = LLONG_MAX;
-
-            for (Job* job : u1)
-                min_process_time = min(min_process_time, job->ptime);
-
-            for (Job* job : u2)
-                min_due_date = min(min_due_date, job->dDate);
-
-            vector<Job*> gamma_lambda;
-            for (Job* job : u1) 
-                if (job->ptime == min_process_time)
-                    gamma_lambda.push_back(job);
-
-            for (Job* job : u2) 
-                if (job->dDate == min_due_date) 
-                    gamma_lambda.push_back(job);
-            
-            // find the job with the minimum MDD value from gamma and lambda
-            pair<Job*, long long> min_mdd_job = find_min_mdd_job(gamma_lambda, completion_times[machine]);
-
-            if (min_mdd_job.second < min_delta) {
-                min_delta = min_mdd_job.second;
-                min_delta_machine = machine;
-                min_delta_job = min_mdd_job.first;
-            }
-            
-        }
+        // find the job with the minimum MDD value from the unscheduled jobs
+        find_gl(jobs, numMachines, completion_times,  min_delta_machine, min_delta_job); 
 
         // schedule the job with the minimum MDD value on the machine with the minimum delta
         schedules[min_delta_machine].push_back(min_delta_job);
@@ -133,6 +137,6 @@ vector<vector<Job*>> mddScheduling(vector<Job *> jobs, int numMachines) {
             }
         }
     }
-
     return schedules;
 } 
+
