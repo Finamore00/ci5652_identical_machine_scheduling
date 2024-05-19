754cbde7af13b7ef2d569fe39d2df90b931bef75
Ana <73360276+anna-ayn@users.noreply.github.com>
2024-05-19 15:51:38 -0400

    script for results

--- results/eachtest.sh
+++ results/eachtest.sh
@@ -33,3 +33,37 @@ for file in ./n=20/*hresults.txt
         ./total_tardiness.sh $file > ttH$filename.txt
         ./timeh.sh $file > timeH$filename.txt
     done
+
+echo "/n"
+echo "n = 25" 
+echo "Procesando archivos de resultados de ls heuristic"
+for file in ./n=25/*results_lsheuristic.txt
+    do
+        filename="${file##*/}"
+        filename="${filename%.*}"
+        echo "Procesando archivo: $filename"
+        ./total_tardiness.sh $file > ttLSH$filename.txt
+        ./timels.sh $file > timeLSH$filename.txt
+    done
+
+# ls random
+echo "Procesando archivos de resultados de ls random"
+for file in ./n=25/*results_lsrandom.txt
+    do
+        filename="${file##*/}"
+        filename="${filename%.*}"
+        echo "Procesando archivo: $filename"
+        ./total_tardiness.sh $file > ttLSrandom$filename.txt
+        ./timels.sh $file > timeLSrandom$filename.txt
+    done
+
+# heuristic
+echo "Procesando archivos de resultados de heuristic"
+for file in ./n=25/*hresults.txt
+    do
+        filename="${file##*/}"
+        filename="${filename%.*}"
+        echo "Procesando archivo: $filename"
+        ./total_tardiness.sh $file > ttH$filename.txt
+        ./timeh.sh $file > timeH$filename.txt
+    done
