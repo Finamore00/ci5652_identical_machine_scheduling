2d893a44ee6f153bcd47eb55e66802069ac99cc8
Ana <73360276+anna-ayn@users.noreply.github.com>
2024-06-03 14:17:39 -0400

    separate results

--- run_genetic_algorithm.sh
+++ run_genetic_algorithm.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+mkdir -p results/genetic_results2
+mkdir -p results/genetic_results2/n=20
+mkdir -p results/genetic_results2/n=25
+
 export -f myfunc
 
 myfunc() {
     n=$1
     m=$2
     echo "Benchmark Started. m=$m, n=$n"
-    target/PROY2 benchmarks/n="$n"/m="$m" 5 > results/genetic_results/n="$n"/"$n"-"$m"results_genetic.txt
+    target/PROY2 benchmarks/n="$n"/m="$m" 5 > results/genetic_results2/n="$n"/"$n"-"$m"results_genetic.txt
 }
 
 export -f myfunc
