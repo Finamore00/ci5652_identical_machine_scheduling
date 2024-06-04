582ae78ea3d354577603ad6bac939dd936c7bed0
Ana <73360276+anna-ayn@users.noreply.github.com>
2024-06-04 10:39:37 -0400

    SA prueba 1 ready

--- run_simulated_annealing.sh
+++ run_simulated_annealing.sh
@@ -4,7 +4,7 @@ myfunc() {
     n=$1
     m=$2
     echo "Benchmark Started. m=$m, n=$n"
-    target/PROY2 benchmarks/n="$n"/m="$m" 3 > results/simulated_annealing_results/n="$n"/"$n"-"$m"results_simulated_annealing.txt
+    target/PROY2 benchmarks/n="$n"/m="$m" 3 > ./results/simulated_annealing_results_1/n="$n"/"$n"-"$m"results_simulated_annealing.txt
 }
 
 export -f myfunc
