0e44397a9a186567c9d3448ffe05cdfb8901dee5
Ana <73360276+anna-ayn@users.noreply.github.com>
2024-06-19 14:59:36 -0400

    meme1

--- run_memetic_algorithm.sh
+++ run_memetic_algorithm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mkdir -p results/memetic_results
-mkdir -p results/memetic_results/n=20
-mkdir -p results/memetic_results/n=25
+mkdir -p results/memetic_results2
+mkdir -p results/memetic_results2/n=20
+mkdir -p results/memetic_results2/n=25
 
 export -f myfunc
 
@@ -10,7 +10,7 @@ myfunc() {
     n=$1
     m=$2
     echo "Benchmark Started. m=$m, n=$n"
-    target/PROY3 benchmarks/n="$n"/m="$m" 1 > results/memetic_results/n="$n"/"$n"-"$m"results_memetic.txt
+    target/PROY3 benchmarks/n="$n"/m="$m" 1 > results/memetic_results2/n="$n"/"$n"-"$m"results_memetic2.txt
 }
 
 export -f myfunc
