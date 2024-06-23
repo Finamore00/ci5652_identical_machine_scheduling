f828fd4d8d7b705efc3c64802238c341cc17ddfc
Jeam <19-10234@usb.ve>
2024-06-23 13:29:09 -0400

    Adds ACO results 3

--- run_aco_algorithm.sh
+++ run_aco_algorithm.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-mkdir -p results/aco_results2
-mkdir -p results/aco_results2/n=20
-mkdir -p results/aco_results2/n=25
+mkdir -p results/aco_results3
+mkdir -p results/aco_results3/n=20
+mkdir -p results/aco_results3/n=25
 
 myfunc() {
     n=$1
     m=$2
     echo "Benchmark Started. m=$m, n=$n"
-    target/PROY3 benchmarks/n="$n"/m="$m" 3 > results/aco_results2/n="$n"/"$n"-"$m"results_aco2.txt
+    target/PROY3 benchmarks/n="$n"/m="$m" 3 > results/aco_results3/n="$n"/"$n"-"$m"results_aco3.txt
 }
 
 export -f myfunc
