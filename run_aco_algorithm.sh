b8563f63211782a9706e21b6341692dea0dca3e8
Jeam <19-10234@usb.ve>
2024-06-23 21:15:11 -0400

    Update ACO results folder to version 6

--- run_aco_algorithm.sh
+++ run_aco_algorithm.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-mkdir -p results/aco_results5
-mkdir -p results/aco_results5/n=20
-mkdir -p results/aco_results5/n=25
+mkdir -p results/aco_results6
+mkdir -p results/aco_results6/n=20
+mkdir -p results/aco_results6/n=25
 
 myfunc() {
     n=$1
     m=$2
     echo "Benchmark Started. m=$m, n=$n"
-    target/PROY3 benchmarks/n="$n"/m="$m" 3 > results/aco_results5/n="$n"/"$n"-"$m"results_aco5.txt
+    target/PROY3 benchmarks/n="$n"/m="$m" 3 > results/aco_results6/n="$n"/"$n"-"$m"results_aco6.txt
 }
 
 export -f myfunc
